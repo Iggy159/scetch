6cdbfe839a9fbe19020cc4bb01b9d6742dc5ffce
Iggy159 <georgebazhenov27@gmail.com>
2022-03-21 23:04:42 +0300

    text_async

--- sketch-06.js
+++ sketch-06.js
@@ -4,22 +4,53 @@ const settings = {
   dimensions: [ 1080, 1080 ]
 };
 
+let manager
+
+let text = 'X'
+let fontSize = 1200
+let fontFamily = 'Droid Serif'
+
 const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
     context.fillStyle = 'black'
-    context.font = '1200px serif'
-    context.textBaseline = 'middle'
-    context.textAlign = 'center'
+    //context.font = fontSize + 'px' + fontFamily
+    context.font = `${fontSize}px ${fontFamily}`
+    context.textBaseline = 'top'
+    //context.textAlign = 'center'
+
+    const metrics = context.measureText(text)
+    const mx = metrics.actualBoundingBoxLeft * -1
+    const my = metrics.actualBoundingBoxAscent * -1
+    const mw = metrics.actualBoundingBoxLeft + metrics.actualBoundingBoxRight
+    const mh = metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent
+
+    const x = (width - mw) * 0.5 - mx
+    const y = (height - mh) * 0.5 - my
 
     context.save()
-    context.translate(width * 0.5, height * 0.5)
+    context.translate(x, y)
 
-    context.fillText('E', 0, 0)
+    context.beginPath()
+    context.rect(mx, my, mw, mh)
+    context.stroke()
+
+    context.fillText(text, 0, 0)
     context.restore()
   };
 };
 
-canvasSketch(sketch, settings);
+const onKeyUp = (e) => {
+  text = e.key.toUpperCase()
+  manager.render()
+}
+
+document.addEventListener('keyup', onKeyUp)
+
+const start = async () => {
+  manager = await canvasSketch(sketch, settings);
+}
+
+start()
